f898508b9a0b52c8f24ebf877492e228382d5d3a
freshmediaro <justmearg@yahoo.com>
2025-05-19 08:19:37 +0300

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -9,6 +9,16 @@ function toggleWidgetsScreen() {
 }
 // To test, call from console: toggleWidgetsScreen()
 
+// if no Webkit browser
+(function(){
+  let isChrome = /Chrome/.test(navigator.userAgent) && /Google Inc/.test(navigator.vendor);
+  let isSafari = /Safari/.test(navigator.userAgent) && /Apple Computer/.test(navigator.vendor);
+  let scrollbarDiv = document.querySelector('.scrollbar');
+    if (!isChrome && !isSafari) {
+      scrollbarDiv.innerHTML = 'You need Webkit browser to run this code';
+    }
+})();
+
 document.addEventListener('DOMContentLoaded', function() {
   console.log('SCRIPT EXECUTION: DOMContentLoaded START');
 
