1f5ada19ceb7d89cfdc38ae076792c2837170ca1
freshmediaro <justmearg@yahoo.com>
2025-08-24 16:32:26 +0300

    Delete tenantos-vue directory

--- tenantos-vue/src/main.ts
+++
@@ -1,9 +0,0 @@
-import { createApp } from 'vue'
-import './style.css'
-import './assets/global.css'
-import './assets/product-content.css'
-import './assets/onboarding.css'
-import App from './App.vue'
-import router from './router'
-
-createApp(App).use(router).mount('#app')
